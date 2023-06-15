f5205e4043b76025a6a9f8486ddc1385ff858b96
hiddartho <56486358+hiddartho@users.noreply.github.com>
2023-06-15 19:43:06 +0530

    Added one Component Composition

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 // import React from "react";
+const Heading = () => <h1>Yeah, I am on the verge to it, of coming out</h1>;
+
 export const App = () => {
   return (
     <>
-      <h2>did you get alive</h2>
+      <div>
+        <h2>did you get alive</h2>
+        <Heading />
+      </div>
     </>
   );
 };
